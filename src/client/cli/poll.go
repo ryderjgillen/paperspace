4e9efdc4198033c9504d5d9d9acc74154e874726
Ryder Gillen <ryder.gillen@tomra.com>
2022-03-14 15:07:43 +0000

    - add poll command defaults - optionally display /metrics

--- src/client/cli/poll.go
+++ src/client/cli/poll.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"io/ioutil"
+	"net/http"
 	"time"
 
 	client "portService/grpc"
@@ -13,11 +15,12 @@ import (
 )
 
 type PollCmd struct {
-	Interval    time.Duration
-	Address     string
-	Port        int `kong:"required,default='50051'"`
-	PromAddress string
-	PromPort    int
+	Interval    time.Duration `kong:"default='15s'"`
+	Address     string        `kong:"default=''"`
+	Port        int           `kong:"default='59001'"`
+	PromAddress string        `kong:"default=''"`
+	PromPort    int           `kong:"default='59002'"`
+	ShowMetrics bool          `kong:"default='false'"`
 }
 
 func (cmd *PollCmd) Run(_ *Context) error {
@@ -28,10 +31,45 @@ func (cmd *PollCmd) Run(_ *Context) error {
 	defer conn.Close()
 
 	c := client.NewPortInfoClient(conn)
+	for {
+
+		err := displayPortInfo(c)
+		if err != nil {
+			return err
+		}
+
+		if cmd.ShowMetrics {
+			err = displayMetrics(cmd.PromAddress, cmd.PromPort)
+			if err != nil {
+				return err
+			}
+		}
+
+		time.Sleep(cmd.Interval)
+	}
+}
+
+func displayPortInfo(c client.PortInfoClient) error {
+
+	formatPort := func(port uint32) string {
+		if port == 0 {
+			return "*"
+		}
+
+		return fmt.Sprintf("%d", port)
+	}
+
+	truncateText := func(s string, max int) string {
+		if max > len(s) {
+			return s
+		}
+		return fmt.Sprintf("%s...", s[:max])
+	}
 
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
 
+	fmt.Printf("**************************************** %s: %s ****************************************\n\n", "Polling", time.Now().UTC())
 	r, err := c.GetPortInfo(ctx, &client.PortInfoRequest{})
 	if err != nil {
 		return fmt.Errorf("ERROR: %s\n", err.Error())
@@ -47,19 +85,34 @@ func (cmd *PollCmd) Run(_ *Context) error {
 
 			fmt.Printf("Protocol=%s, ", portInfo.Protocol.String())
 			fmt.Printf("Source.IpAddress=%s:%s, ", portInfo.Source.IpAddress, formatPort(portInfo.Source.Port))
-			fmt.Printf("Destination.IpAddress=%s:%s", portInfo.Destination.IpAddress, formatPort(portInfo.Destination.Port))
+			fmt.Printf("Destination.IpAddress=%s:%s, ", portInfo.Destination.IpAddress, formatPort(portInfo.Destination.Port))
+			fmt.Printf("Command=%s", truncateText(portInfo.Command, 50))
 
 			fmt.Println()
 		}
 	}
 
+	fmt.Println()
+	fmt.Println()
+
 	return nil
 }
 
-func formatPort(port uint32) string {
-	if port == 0 {
-		return "*"
+func displayMetrics(address string, port int) error {
+
+	fmt.Printf("**************************************** %s: %s ****************************************\n\n", "Metrics", time.Now().UTC())
+
+	resp, err := http.Get(fmt.Sprintf("http://%s:%d/metrics", address, port))
+	if err != nil {
+		return err
 	}
 
-	return fmt.Sprintf("%d", port)
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(string(body))
+
+	return nil
 }
