2915d5c7784b8c5e85d7ca26690f0a1f673ea70b
Ryder Gillen <ryder.gillen@tomra.com>
2022-03-14 15:50:21 +0000

    netlink

---
+++ src/service/netlink/netlink.go
@@ -0,0 +1,63 @@
+package netlink
+
+import (
+	"os"
+
+	"github.com/kckeiks/netlink"
+	"github.com/kckeiks/netlink/sockdiag"
+	"golang.org/x/sys/unix"
+)
+
+func SendInetMessage(nlmsg []byte) ([]sockdiag.InetDiagMsg, error) {
+	fd, err := unix.Socket(unix.AF_NETLINK, unix.SOCK_RAW, unix.NETLINK_SOCK_DIAG)
+	if err != nil {
+		return nil, err
+	}
+
+	addr := &unix.SockaddrNetlink{Family: unix.AF_NETLINK}
+	unix.Sendto(fd, nlmsg, 0, addr)
+
+	nlmsgs, err := receiveNetlinkMessage(fd)
+	if err != nil {
+		return nil, err
+	}
+
+	var idmsgs []sockdiag.InetDiagMsg
+	for _, msg := range nlmsgs {
+		inetMsg, _ := sockdiag.DeserializeInetDiagMsg(msg.Payload)
+		idmsgs = append(idmsgs, *inetMsg)
+	}
+
+	return idmsgs, nil
+}
+
+func receiveNetlinkMessage(fd int) ([]netlink.NetlinkMessage, error) {
+	nlmsgs := make([]netlink.NetlinkMessage, 0)
+	for done := false; !done; {
+
+		b := make([]byte, os.Getpagesize())
+		r, _, _ := unix.Recvfrom(fd, b, 0)
+		if r == 0 {
+			return nlmsgs, nil
+		}
+
+		parsedMsgs, err := netlink.ParseNetlinkMessage(b[:r])
+		if err != nil {
+			return nil, err
+		}
+
+		for _, msg := range parsedMsgs {
+			if msg.Header.Type == unix.NLMSG_DONE {
+				done = true
+				break
+			}
+
+			if msg.Header.Type == unix.NLMSG_ERROR {
+				return nil, netlink.NlMsgHeaderError
+			}
+			nlmsgs = append(nlmsgs, msg)
+		}
+	}
+
+	return nlmsgs, nil
+}
